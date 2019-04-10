25bc0dc82fc70fcd99fd275b79eee7cac4a2b314
JPdiedhiou <diedhioujp@outlook.com>
2019-04-10 14:46:05 +0000

    first commit

--- valeur_aleatoire.py
+++ valeur_aleatoire.py
@@ -1,14 +1,6 @@
-#!Generer un nombre aléatoire
-import random
-nbre_alea = random.randint(0,9)
-rep_user = None
-#!Entrer le nombre de comparaison
-while rep_user != nbre_alea:
-      rep_user =int(input("Saisir un nombre entre 0 et 9:"))
-#!Comparaison avec le nombre génèré
-      if rep_user > nbre_alea:
-            print("Désolé le nombre est plus petit")
-      elif rep_user < nbre_alea:
-            print("Désolé le nombre est plus grand")
-      else:
-            print("Bravo! Tu as trouvé le nombre")
+x=int(input("Saisir une valeur compris entre 0 et 9 :"))
+
+if x >= 0 and x <= 9 :
+    print("Bravo! Vous avez trouvé")
+else:
+    print("Désolé réessayer")
