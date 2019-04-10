dbbf66fa429fd3cdd1bb87e3227c858a815f0882
JPdiedhiou <diedhioujp@outlook.com>
2019-04-10 10:50:06 +0000

    first commit

--- pythontest.py
+++ pythontest.py
@@ -1,7 +1,6 @@
-n=int(input("Entrer le nombre de valeur à entrer:"))
-a=[]
-for i in range(0,n):
-        elem=int(input("Entrer une valeur: "))
-        a.append(elem)
-avg=sum(a)/n
-print("La moyenne des valeurs est", round(avg,2))
+#Entrer les valeurs des nombres
+x=int(input("Entrer la  valeur de x:"))
+y=int(input("Entrer la valeur de y:"))
+#Calcule de la moyenne 
+m=int((x + y)/2)       
+print("La moyenne des valeurs est:",m)
